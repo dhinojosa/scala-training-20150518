28b4ea868da9e0c6cb6014275e96cce3e1390954
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2015-05-19 14:06:12 -0600

    removed files

--- UsingClasses.scala
+++
@@ -1,9 +0,0 @@
-class Employee(val firstName:String, var lastName:String) {
-//   def fullName:String = {
-//     s"${firstName} ${lastName}"
-//   }
-
-     def fullName = s"$firstName $lastName"
-}
-
-
